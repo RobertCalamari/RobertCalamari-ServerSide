452bfa37155633a08fa4a48d4d8af62b224c3e1b
Robert <rjcalamari@gmail.com>
2023-05-19 15:22:40 -0400

    first commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
@@ -20,6 +20,7 @@ io.on("connection", (socket) => {
 
   socket.on("join_room", (data) => {
     socket.join(data);
+    console.log(socket.id + ' - Room Joined: ' + data);
   });
 
   socket.on("send_message", (data) => {
