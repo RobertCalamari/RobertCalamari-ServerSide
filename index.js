def0427f02ec84c4dc52ceedf9ca26d2037440ba
Robert <rjcalamari@gmail.com>
2023-05-19 15:06:50 -0400

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const path = require('path');
-const http = require('http');
+const express = require("express");
+const app = express();
+const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 
@@ -9,13 +9,24 @@ app.use(cors());
 const server = http.createServer(app);
 
 const io = new Server(server, {
-
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
-    }
-
-
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
-server.listen(process.env.PORT || 8000);
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("SERVER IS RUNNING");
+});
