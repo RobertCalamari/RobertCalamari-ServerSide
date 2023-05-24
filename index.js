bfb83727edcb1d546323bb6722c61b7321ff953c
Robert <rjcalamari@gmail.com>
2023-05-24 15:27:42 -0400

    first commit

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.use(cors());
 const server = http.createServer(app);
 
 const io = new Server(server, {
+transports: ['websocket'],
+  pingInterval: 1000 * 60 * 5,
+  pingTimeout: 1000 * 60 * 3,
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
